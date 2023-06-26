0f5bfb24d9146f89556462675eca68c59ad3feda
Jakub Bok <58332788+instred@users.noreply.github.com>
2023-06-26 14:54:16 +0200

    Update Main.cpp
    26.06

--- Codewars C++/Main.cpp
+++ Codewars C++/Main.cpp
@@ -343,3 +343,156 @@ public:
   	}
 };
 
+
+/*The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. 
+It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares*/
+
+typedef unsigned long long ull;
+class SumFct
+{
+ 	public:
+  	static ull perimeter(int n){
+		if (n == 0)
+			return 0;
+		n++;
+		ull a = 0, b = 0, ans = 1, curr = 1;
+		for (ull i = 1; i < n; i++){
+			a = b;
+			b = curr;
+			curr = a+b;
+			ans += curr;
+		}
+		return 4 * ans;
+	}
+};
+
+
+/*Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, 
+which is the number of times you must multiply the digits in num until you reach a single digit.*/
+
+typedef long long ll;
+int persistence(ll n){
+	int ans = 0;
+	int len = to_string(n).length();
+	ll curr = 1;
+	while (len > 1){
+		for (int i = 0; i < len; i++){
+			curr *= to_string(n)[i] - '0';
+		}
+		ans ++;
+		n = curr;
+		len = to_string(curr).length();
+		curr = 1;
+	}
+	return ans;
+}
+
+
+/*Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. 
+A tower block is represented with "*" character.
+For example, a tower with 3 floors looks like this:
+[
+  "  *  ",
+  " *** ", 
+  "*****"
+]*/
+
+vector<string> towerBuilder(unsigned nFloors) {
+  	int lastFloor = nFloors * 2 - 1;
+	string floor = "";
+	vector<string> ans;
+	for (int i = 1; i < nFloors+1; i++){
+		floor += string((lastFloor - (i * 2 - 1))/2, ' ');
+		floor += string(i * 2 - 1, '*');
+		floor += string((lastFloor - (i * 2 - 1))/2, ' ');
+		ans.push_back(floor);
+		floor = "";
+	}
+	return ans;	
+}
+
+
+/*Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+Rules for a smiling face:
+Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned.*/
+
+bool validate(string s){
+	int len = s.length();
+	if (len == 2){
+		if (s == ":)" || s == ";)" || s == ":D" || s == ";D"){
+			return true;
+		}
+	}
+	if (len == 3){
+		if ((s[0] == ':' || s[0] == ';') && (s[1] == '-' || s[1] == '~') && (s[2] == 'D' || s[2] == ')'))
+			return true;
+	}
+	return false;
+}
+
+int countSmileys(vector<string> arr)
+{
+	int ans = 0;
+	if (arr.empty()){
+		return ans;
+	}
+ 	for (string s : arr){
+		if(validate(s))
+			ans++;
+	}
+	return ans;
+}
+
+
+//Same task as before but done easier and more compact using Regex
+
+#include <regex>
+
+int countSmileys(vector<string> arr){
+	int ans = 0;
+	regex smileys("[:|;](-|~)?[)|D]");
+	for (auto &s : arr){
+		if(regex_match(s, smileys))
+			ans++;
+	}
+	return ans;
+}
+
+
+/*Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]*/
+
+vector<int> snail(const vector<vector<int>> &snail_map) {
+	if (snail_map.empty() || snail_map[0].empty())
+		return {};
+  	vector<int> ans;
+	int x = 0;
+	int n = snail_map.size();
+	while (x != n){
+		for(int i = x; i <= n-1; i++){
+			ans.push_back(snail_map[x][i]);
+			if(pow(snail_map.size(),2) == ans.size()){
+				return ans;
+			}
+		}
+		for(int i = x+1; i <= n-1; i++){
+			ans.push_back(snail_map[i][n-1]);
+		}
+		for(int i = n-2; i >= x; i--){
+			ans.push_back(snail_map[n-1][i]);
+		}
+		for(int i = n-2; i >= x+1; i--){
+			ans.push_back(snail_map[i][x]);
+		}
+		x++;
+		n--;
+	}
+	return ans;
+}

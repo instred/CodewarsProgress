450430a6c65e7270f3fa3249867e825644cc8670
instred <mrkubson230@gmail.com>
2023-10-23 19:05:13 +0200

    ex3

--- LeetCode-Progress/solution.py
+++ LeetCode-Progress/solution.py
@@ -81,10 +81,33 @@ class Solution:
 sol = Solution()
 print(sol.numIdenticalPairs([1,2,3,1,1,3]))
 
+
+class MyHashMap:
+
+    def __init__(self):
+        self.hashmap = []
+
+    def put(self, key: int, value: int) -> None:
+        self.hashmap.append([key, value])
+
+    def get(self, key: int) -> int:
+        pass
+
+    def remove(self, key: int) -> None:
+        pass
+
+    def show(self) -> List[int]:
+        return self.hashmap
+
 # Design a HashMap without using any built-in hash table libraries.
 # Implement the MyHashMap class:
 # MyHashMap() initializes the object with an empty map.
 # void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
 # int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
 # void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
- 
+
+key, value = 1, 1
+obj = MyHashMap()
+obj.put(key,value)
+obj.put(key+1,value+1)
+print(obj.show())
